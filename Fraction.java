3b5999b64d28c3b6ed70354bc4bf7535fdc4458d
Jonatan Giuseppe Schnyder <jonatan@shade.light>
2024-01-31 08:42:51 +0100

    8. Schlüsselwörter, Aufgabe 4.1

--- Fraction.java
+++ Fraction.java
@@ -1,54 +1,82 @@
 public class Fraction {
-    static int numberOfFractions;
-    private int numerator, denominator;
+	// final double PI = 3.14159;
+	static int numberOfFractions;
+	private int numerator, denominator;
+	static protected float pi = 3.14159f;
 
-    @Override
-    public String toString() {
-	    return this.numerator + "/" + this.denominator;
-    }
+	public static Fraction multiply(Fraction one, Fraction two) {
+		return new Fraction(one.numerator * two.numerator, one.denominator * two.denominator);
+	}
 
-    public Fraction(int numerator, int denominator) {
-	    this.numerator = numerator;
-	    this.denominator = denominator;
-	    numberOfFractions++;
-    }
-    
-    public Fraction(Fraction f) {
-        this(f.numerator, f.denominator);
-    }
+	public static Fraction subtract(Fraction one, Fraction two) {
+		return new Fraction(one.numerator * two.denominator - one.denominator * two.numerator, one.denominator * two.denominator);
+	}
 
-    public Fraction() {
-        this(0, 1);
-    }
+	public static Fraction divide(Fraction one, Fraction two) {
+		return new Fraction(one.numerator * two.denominator, one.denominator * two.numerator);
+	}
 
-    public void setDenominator(int denominator) {
-	    if (denominator == 0) {
-		    throw new ArithmeticException("Division by zero");
-	    } else {
-		    this.denominator = denominator;
-	    }
-    }
 
-    public void setNumerator(int numerator) {
-	    this.numerator = numerator;
-    }
+	public Fraction multiply(Fraction other) {
+		return Fraction.multiply(this, other);
+	}
 
-    public void addTo(Fraction f) {
-        this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
-        this.setDenominator(this.denominator * f.denominator);
-    }
+	public Fraction subtract(Fraction other) {
+		return Fraction.multiply(this, other);
+	}
 
-    public Fraction add(Fraction f) {
-	    return new Fraction(this.numerator * f.denominator + this.denominator * f.numerator, this.denominator * f.denominator);
-    }
+	public Fraction divide(Fraction other) {
+		return Fraction.divide(this, other);
+	}
 
-    public static Fraction add(Fraction f, Fraction g) {
-	    // f.addTo(g);
-	    return f.add(g);
-    }
-    
-    public boolean equals(Fraction f) {
-        return this.numerator * f.denominator == this.denominator * f.numerator;
-    }
+	@Override
+	public String toString() {
+		return this.numerator + "/" + this.denominator;
+	}
+
+	public Fraction(int numerator, int denominator) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+		numberOfFractions++;
+	}
+
+	public Fraction(Fraction f) {
+		this(f.numerator, f.denominator);
+	}
+
+	public Fraction() {
+		this(0, 1);
+	}
+
+	public void setDenominator(int denominator) {
+		if (denominator == 0) {
+			throw new ArithmeticException("Division by zero");
+		} else {
+			this.denominator = denominator;
+		}
+	}
+
+	public void setNumerator(int numerator) {
+		this.numerator = numerator;
+	}
+
+	public void addTo(Fraction f) {
+		this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
+		this.setDenominator(this.denominator * f.denominator);
+	}
+
+	public Fraction add(Fraction f) {
+		return new Fraction(this.numerator * f.denominator + this.denominator * f.numerator,
+			       	this.denominator * f.denominator);
+	}
+
+	public static Fraction add(Fraction f, Fraction g) {
+		// f.addTo(g);
+		return f.add(g);
+	}
+
+	public boolean equals(Fraction f) {
+		return this.numerator * f.denominator == this.denominator * f.numerator;
+	}
 
 }
--- FractionTester.java
+++ FractionTester.java
@@ -1,6 +1,11 @@
 public class FractionTester {
     public static void main(String[] args) {
         Fraction f = new Fraction();
+
+	System.out.println(Fraction.pi);
+	System.out.println(f.pi);
+	// f.PI =5;
+
         // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
         System.out.println(f);
         f.setNumerator(12);
@@ -19,6 +24,12 @@ public class FractionTester {
 	System.out.println((b.equals(f)) ? "b === f" : "b !== f");
         
         // Klassen- und Instanzenmethoden:
+	
+	Fraction a4 = new Fraction(2,3);
+	Fraction b4 = new Fraction(3,2);
+
+	System.out.println(Fraction.multiply(a4, b4));
+	System.out.println(a4.multiply(b4));
 
         // statische Variablen:
 
